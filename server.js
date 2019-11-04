620ca11c5c6ab7ce6db58dd99df833a8feb7507b
Erdinc <Erdinc@gklfmg.com>
2019-11-04 23:13:15 +0100

    session passport and strategy added with config

--- server.js
+++ server.js
@@ -3,14 +3,34 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const validator = require("validator");
 const mongoose = require("mongoose");
+const fs = require('fs');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const TwitterStrategy = require('passport-twitter').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 const config = require('./config/config');
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(session({
+    secret: "erdinc",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(config.connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, min: 8 },
@@ -19,6 +39,9 @@ const userSchema = new mongoose.Schema({
     gender: { type: String, enum: ["Male", "Female", "Others"] },
     dob: { type: Date }
 });
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
@@ -31,13 +54,22 @@ const postSchema = new mongoose.Schema({
         username: { type: String, required: true, min: 8 }
     })
 });
+postSchema.plugin(passportLocalMongoose);
+postSchema.plugin(findOrCreate);
 
 const Post = mongoose.model("Post", postSchema);
 
+passport.use(User.createStrategy());
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
 
 app.get("/", (req, res) => {
     res.render("login");
@@ -47,7 +79,7 @@ app.post("/home", (req, res) => {
     let usernameInput = req.body.username;
     let passwordInput = req.body.password;
 
-    if (validator.isEmpty(usernameInput) || validator.isEmpty(passwordInput)) {
+    if (!validator.isEmpty(usernameInput) || !validator.isEmpty(passwordInput)) {
         res.send("Username and Password cannot be empty");
     } else {
         User.findOne(
@@ -55,7 +87,6 @@ app.post("/home", (req, res) => {
             (err, foundUser) => {
                 if (!err) {
                     if (foundUser) {
-                        
                         res.redirect("/home");
                     } else {
                         res.send("Username or Password not Correct");
@@ -69,7 +100,6 @@ app.post("/home", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-
     let usernameNew = req.body.username;
     let passwordNew = req.body.password;
     let genderNew = req.body.gender;
@@ -108,21 +138,30 @@ app.post("/register", (req, res) => {
     } else { console.log('5'); }
 });
 
-app.get("home/:id", (req, res) => {
-    let newPostId = req.params;
-    let newComment = req.body.comment;
+app.router("home/:id")
+    .get((req, res) => {
+        let newPostId = req.params;
+        let newComment = req.body.comment;
 
-    if (validator.isEmpty(newComment, { max: 500 })) {
-        res.send("Empty Post!!!");
-    } else {
-        const newPost = new Post({
-            comment: newComment
+        if (validator.isEmpty(newComment, { max: 500 })) {
+            res.send("Empty Post!!!");
+        } else {
+            const newPost = new Post({
+                comment: newComment
+            });
+            newPost.save();
+            newPostId = Post.findById(newPost._id, { lean: true });
+            res.redirect("home/:id");
+        }
+    })
+    .delete((req, res) => {
+        let id = req.params;
+        Post.deleteOne({ _id: id }, (err) => {
+            if (!err) {
+                res.redirect("/home");
+            }
         });
-        newPost.save();
-        newPostId = Post.findById(newPost._id, { lean: true });
-        res.redirect("home/:id");
-    }
-});
+    });
 
 app.get("home/like/:id", (req, res) => {
     let id = req.params;
@@ -142,14 +181,6 @@ app.get("home/dislike/:id", (req, res) => {
     });
 });
 
-app.delete("home/:id", (req, res) => {
-    let id = req.params;
-    Post.deleteOne({ _id: id }, (err) => {
-        if (!err) {
-            res.redirect("/home");
-        }
-    });
-});
 
 app.listen(3000, function () {
     console.log(`Server started perfectly on ${config.port}...`);
